c0bbe6aa16e96432a6d7a12a8007a291993b672d
Javier Frias <javier.foe@gmail.com>
2021-01-27 12:55:56 +0100

    ADD Merging in all directions

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,8 +2,36 @@
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    TwentyFortyeight game = new TwentyFortyeight();
+
+    private void Start()
     {
+        Debug.Log(game);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            game.Merge(Direction.Right);
+            Debug.Log(game);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            game.Merge(Direction.Left);
+            Debug.Log(game);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            game.Merge(Direction.Up);
+            Debug.Log(game);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            game.Merge(Direction.Down);
+            Debug.Log(game);
+        }
+    }
+
 }
--- Assets/TwentyFortyeight.cs
+++ Assets/TwentyFortyeight.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
 
 public enum Direction
 {
@@ -13,14 +13,15 @@ public class TwentyFortyeight
 {
 
     public int Score { get; private set; }
-    public int this [int i,int j] => matrix[i,j];
+    public int this[int i, int j] => matrix[i, j];
 
     private readonly int[,] matrix = new int[4, 4];
     private readonly List<int> emptySquares = new List<int>();
+    private readonly static Random random = new Random();
 
     public TwentyFortyeight()
     {
-        for(int i = 0; i < 16; i++)
+        for (int i = 0; i < 16; i++)
         {
             emptySquares.Add(i);
         }
@@ -30,14 +31,14 @@ public class TwentyFortyeight
 
     public void GenerateNewNumber()
     {
-        int twoOrFour = Random.Range(0, 10);
+        int twoOrFour = random.Next(0, 10);
         int newNumber = 4;
         //90% => 2, 10% => 4
-        if(twoOrFour > 0)
+        if (twoOrFour > 0)
         {
             newNumber = 2;
         }
-        int randomPosition = Random.Range(0, emptySquares.Count);
+        int randomPosition = random.Next(0, emptySquares.Count);
         int position = emptySquares[randomPosition];
         int xPosition = position / 4;
         int yPosition = position % 4;
@@ -49,7 +50,7 @@ public class TwentyFortyeight
     public void Merge(Direction direction)
     {
         bool reverse = direction == Direction.Down || direction == Direction.Right;
-        if(direction == Direction.Up || direction == Direction.Down)
+        if (direction == Direction.Up || direction == Direction.Down)
         {
             MergeVertically(reverse);
         }
@@ -57,26 +58,134 @@ public class TwentyFortyeight
         {
             MergeHorizontally(reverse);
         }
-    }
-
-    private void MergeVertically(bool reverse)
-    {
-
+        GenerateNewNumber();
     }
 
     private void MergeHorizontally(bool reverse)
     {
+        int start = reverse ? 3 : 0;
+        int interval = reverse ? -1 : 1;
 
+        int freeSpot;
+        //Merging 4 rows
+        for (int i = 0; i < 4; i++)
+        {
+            freeSpot = start;
+            //Moving individual cells
+            for (int j = start; j < 4 && j > -1 && freeSpot < 4 && freeSpot > -1; j += interval)
+            {
+                if (matrix[i, j] == 0) continue;
+                //Empty square to move to
+                if (matrix[i, freeSpot] == 0)
+                {
+                    //Move the value
+                    RemoveFreeSpace(i, freeSpot, matrix[i, j]);
+                    AddFreeSpace(i, j);
+                }
+                //Merging
+                else if (matrix[i, j] == matrix[i, freeSpot] && j != freeSpot)
+                {
+                    int merge = matrix[i, freeSpot] * 2;
+                    matrix[i, freeSpot] = merge;
+                    Score += merge;
+                    freeSpot += interval;
+                    AddFreeSpace(i, j);
+                }
+                //Slide the value to the next empty cell
+                else if (matrix[i, j] != matrix[i, freeSpot])
+                {
+                    freeSpot += interval;
+                    RemoveFreeSpace(i, freeSpot, matrix[i, j]);
+                    if (freeSpot != j)
+                    {
+                        AddFreeSpace(i, j);
+                    }
+                }
+            }
+        }
+    }
+
+    private void MergeVertically(bool reverse)
+    {
+        int start = reverse ? 3 : 0;
+        int interval = reverse ? -1 : 1;
+
+        int freeSpot;
+        //Merging 4 rows
+        for (int j = 0; j < 4; j++)
+        {
+            freeSpot = start;
+            //Moving individual cells
+            for (int i = start; i < 4 && i > -1 && freeSpot < 4 && freeSpot > -1; i += interval)
+            {
+                if (matrix[i, j] == 0) continue;
+                //Empty square to move to
+                if (matrix[freeSpot, j] == 0)
+                {
+                    //Move the value
+                    RemoveFreeSpace(freeSpot, j, matrix[i, j]);
+                    AddFreeSpace(i, j);
+                }
+                //Merging
+                else if (matrix[i, j] == matrix[freeSpot, j] && i != freeSpot)
+                {
+                    int merge = matrix[freeSpot, j] * 2;
+                    matrix[freeSpot, j] = merge;
+                    Score += merge;
+                    freeSpot += interval;
+                    AddFreeSpace(i, j);
+                }
+                //Slide the value to the next empty cell
+                else if (matrix[i, j] != matrix[freeSpot, j])
+                {
+                    freeSpot += interval;
+                    RemoveFreeSpace(freeSpot, j, matrix[i, j]);
+                    if (freeSpot != i)
+                    {
+                        AddFreeSpace(i, j);
+                    }
+                }
+            }
+        }
+    }
+
+    private void Merge(int i, int j, int interval, ref int freeSpot)
+    {
+    }
+
+    private void AddFreeSpace(int i, int j)
+    {
+        matrix[i, j] = 0;
+        int position = From2Dto1D(i, j);
+        if (!emptySquares.Contains(position))
+        {
+            emptySquares.Add(position);
+        }
+    }
+
+    private void RemoveFreeSpace(int i, int j, int value)
+    {
+        matrix[i, j] = value;
+        int position = From2Dto1D(i, j);
+        if (emptySquares.Contains(position))
+        {
+            emptySquares.Remove(position);
+        }
+    }
+
+    private int From2Dto1D(int i, int j)
+    {
+        return i * 4 + j;
     }
 
     public override string ToString()
     {
-        string result = "\n";
-        for(int i = 0; i < 4; i++)
+        string result = "Score: " + Score + "\tEmpty squares: " + emptySquares.Count + "\n";
+        for (int i = 0; i < 4; i++)
         {
-            for(int j = 0; j < 4; j++)
+            for (int j = 0; j < 4; j++)
             {
-                result += matrix[i, j] + " ";
+                result += matrix[i, j] + "\t";
             }
             result += "\n";
         }
